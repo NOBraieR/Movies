7f6cea1ea881707bd61d6c094fc3fb6f3e643aac
Oozy02 <sahilkakd18@gmail.com>
2021-02-19 15:11:13 +0530

    first commit

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ const PORT = 3000;
 app.set('view engine' , 'html');
 
 app.get('/' , (req,res,next)=>{
-  console.log(req.headers);
+
   res.sendFile(path.join(__dirname+'/index.html' ))
+});
+
+app.get('/first' , (req,res,next)=>{
+
+  res.sendFile(path.join(__dirname+'/first.html' ))
 });
 
 app.listen(PORT , (error)=>{
